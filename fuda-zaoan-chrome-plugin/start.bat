ef81849b008ba8479722baa36c5fd7a7faf2a74d
heiliuer <heiliuer@github.com>
2016-12-15 12:15:47 +0800

    fixed

---
+++ fuda-zaoan-chrome-plugin/start.bat
@@ -0,0 +1,22 @@
+@echo off
+set target="http://zao.fzu4.com/App/"
+
+set "chrome="
+for /f "tokens=2,*" %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Clients\StartMenuInternet\Google Chrome\shell\open\command" /ve 2^>nul') do set "chrome=%%~B"
+if not defined chrome for /f "tokens=2,*" %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Clients\StartMenuInternet\Google Chrome\shell\open\command" /ve 2^>nul') do set "chrome=%%~B"
+if not defined chrome goto no-chrome
+if defined chrome goto start
+
+:start
+set chrome="%chrome%"
+echo Success open chrome
+::set chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+%chrome%  %target% --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36 MicroMessenger/4.5.255"
+goto end
+
+:no-chrome
+echo Google Chrome was not found.
+
+:end
+echo auto exiting...
+ping 127.1 -n 3 >nul
